9e9d737a5fed76e5f237dfab5eb1087391f6e37a
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-12-15 14:47:37 +0100

    asd

--- js/app.js
+++ js/app.js
@@ -9,11 +9,44 @@ function displayTable() {
     // Calculate the total balance by adding the new amount to the existing balance (assuming the initial balance is 0)
     var balance = 0 + parseInt(amount);
   
-    // Create the HTML for the table
-    var tableHtml = "<table><tr><th>Name</th><th>Description</th><th>Date</th><th>Amount (in EUR)</th><th>Total Balance (in EUR)</th></tr>";
-    tableHtml += "<tr><td>" + name + "</td><td>" + description + "</td><td>" + date + "</td><td>" + amount + "</td><td>" + balance + "</td></tr>";
-    tableHtml += "</table>";
+    // Create the HTML for a new table row with the user's input
+    var tableRowHtml = "<tr><td>" + name + "</td><td>" + description + "</td><td>" + date + "</td><td>" + amount + "</td><td>" + balance + "</td></tr>";
   
-    // Display the table
-    document.getElementById("table").innerHTML = tableHtml;
+    // Get the existing table from the page
+    var tableElement = document.getElementById("myTable");
+    if (!tableElement) {
+      // If the table doesn't exist, create it
+      tableElement = document.createElement("table");
+      tableElement.id = "myTable";
+      tableElement.innerHTML = "<tr><th>Name</th><th>Description</th><th>Date</th><th>Amount (in EUR)</th><th>Total Balance (in EUR)</th></tr>";
+      document.getElementById("table").appendChild(tableElement);
+    }
+  
+    saveData();
+    // Add the new table row to the existing table
+    tableElement.innerHTML += tableRowHtml;
+  }
+
+  // This function gets the values from the form inputs and saves them in localStorage
+function saveData() {
+    // Get the values from the form inputs
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+    var date = document.getElementById("date").value;
+    var amount = document.getElementById("amount").value;
+  
+    // Calculate the total balance by adding the new amount to the existing balance (assuming the initial balance is 0)
+    var balance = 0 + parseInt(amount);
+  
+    // Save the user's input in localStorage
+    localStorage.setItem("name", name);
+    localStorage.setItem("description", description);
+    localStorage.setItem("date", date);
+    localStorage.setItem("amount", amount);
+    localStorage.setItem("balance", balance);
+  }
+
+  if ("serviceWorker" in navigator) {
+    // Register the service worker if the browser supports it
+    navigator.serviceWorker.register("/serviceWorker.js");
   }
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,32 +1,32 @@
-const staticDevCoffee = "pwa-input-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
-];
+// This is the service worker code
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
-});
+// This is the list of assets that will be cached by the service worker
+var assetsToCache = [
+    "/css/styles.css",
+    "/js/app.js",
+    "/images/logo.jpeg"
+  ];
+  
+  // This is the install event, which is triggered when the service worker is installed
+  self.addEventListener("install", function(event) {
+    // Open a cache and add the assets to it
+    event.waitUntil(
+      caches.open("myCache").then(function(cache) {
+        return cache.addAll(assetsToCache);
+      })
+    );
+  });
+  
+  // This is the fetch event, which is triggered when the service worker receives a network request
+  self.addEventListener("fetch", function(event) {
+    // Respond to the request with the cached asset, if it exists, or with a network request
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request);
+        }
+      })
+    );
+  });
